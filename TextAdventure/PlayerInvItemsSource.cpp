a615187322a0ae51dd1860d2a5ca4ce5eebc37ca
Joey Sachtleben <manjoey7@gmail.com>
2017-08-30 18:10:22 -0500

    Further work towards redoing the inventory system

--- TextAdventure/PlayerInvItemsSource.cpp
+++ TextAdventure/PlayerInvItemsSource.cpp
@@ -3,24 +3,18 @@
 #include <iostream>
 #include <string>
 
-std::string ItemsToStrings(ItemList item)
+std::string ItemsToStrings(ItemList item)  // Converts the internal Item ID's into text strings.
 {
 	switch (item)
 	{
 	case ItemList::GENERIC_HAND:
-	{
 		return "Hand";
 		break;
-	}
 	case ItemList::WEAPONS_SWORD:
-	{
 		return "Sword";
 		break;
-	}
 	default:
-	{
-		std::cout << "ERROR: Something went wrong. Error info: _class_Items.ItemsToStrings.INVALID_ITEM_PASSED" << std::endl;
+		std::cout << "ERROR: Something went wrong. Error info: PlayerInvItems.ItemsToStrings.INVALID_ITEM_PASSED" << std::endl;
 		exit(1);
-	}
 	}
 }
--- TextAdventure/PlayerVars.h
+++ TextAdventure/PlayerVars.h
@@ -18,7 +18,7 @@ private:
 	std::array<int, 10> invObjects = { };   // Player has 10 empty slots by default | What if we want to give him the ability to carry more later in the game? Perhaps make this a dynamically allocated array?
 	bool isFighting;                        // Player doesn't start off fighting something
 	Monsters whatFighting;                  // Initialized with the BlankMonster placeholder; if isFighting is true but BlankMonster is here then something is wrong
-	std::vector<ItemList> inventory; // Makes dynamicially sized array player inv, primarily for future use
+	std::vector<ItemList> inventory;        // Makes dynamicially sized array player inv, primarily for future use
 	int damage;                             // Holds how much damage the player can do
 
 public:
@@ -43,7 +43,7 @@ public:
 		health = health - amount;
 	}
 
-	std::string invObjIDToString(int objID)  // Converts the raw int objID's stored in the player inv into text strings.
+	std::string invObjIDToString(int objID)  // Converts the raw int objID's stored in the player inv into text strings.  | DEPRECIATED: This function is depreciated in favor of the ItemsToString function as part of a redo of the inventory system.
 	{
 		switch (objID)
 		{
@@ -61,38 +61,79 @@ public:
 
 	void PrintPlayerInvStrings()  // Prints out the player inventory as a list of text strings.
 	{
-		for (int invSlot = 0; invSlot < invObjects.size(); ++invSlot)
-			std::cout << "Slot " << invSlot + 1 << ": " << invObjIDToString(invObjects.at(invSlot)) << std::endl;
+		for (int invSlot = 0; invSlot < inventory.size(); ++invSlot)
+			std::cout << "Slot " << invSlot + 1 << ": " << ItemsToStrings(inventory.at(invSlot)) << std::endl;
 	}
-	
-	void AddItemToInv(int objID)  // Adds an item to the player's inventory in the first open slot. If none is open, prompts the player to choose a slot to replace with the new item. | TODO: 1. List what is in the chosen slot and confirm choice 2. Add an option to not replace any slot if full
+
+	void ReplaceItemInInv(ItemList item)  // Replaces an item currently in a player's inventory with a new one.
+	{
+		std::cout << "Do you want to replace another slot's contents with this " << ItemsToStrings(item) << "?\n";  // Asks the player if they want to replace something in their inventory with the new item
+		std::cout << "Y/N: ";
+		std::string responce;
+		std::cin >> responce;
+		if (responce == "Y")  // Takes branch for if they do
+		{
+			std::cout << "Please enter a slot to replace it's contents with this.\n";  // Asks player for slot to replace
+			std::cout << "Slot: ";
+			int slot;
+			std::cin >> slot;
+			std::cout << "Are you sure you want to replace " << ItemsToStrings(inventory.at(slot)) << " with " << ItemsToStrings(item) << "?/n";   // Confirms that player entered the correct slot number
+			std::cout << "Y/N: ";
+			std::string responce2;
+			std::cin >> responce2;
+			std::cout << "\n";
+			if (responce2 == "Y")  // If they did, replace item
+			{
+				inventory.at(slot) = item;
+				std::cout << "You dropped the previous item to replace it with a " << ItemsToStrings(inventory.at(slot)) << ".\n";
+			}
+			else if (responce2 == "N")  // If they didn't, go though the whole process again ( I didn't want to split this into yet another function at every question point)
+			{
+				ReplaceItemInInv(item);
+			}
+			else  // If they gave an invalid answer, tell them this and try again
+			{
+				std::cout << "Please respond with either Y for yes or N for no.\n";
+				ReplaceItemInInv(item);
+			}
+		}
+		else if (responce == "N") // Takes branch for if they player doesn't want to replace something
+		{
+			std::cout << "You put back down the " << ItemsToStrings(item) << ".\n";
+		}
+		else  // If they gave in invalid answer, tell them this and try again
+		{
+			std::cout << "Please respond with either Y for yes or N for no.\n";
+			ReplaceItemInInv(item);
+
+		}
+	}
+
+	void AddItemToInv(ItemList item)  // Adds an item to the players inventory.
 	{
 		bool invIsFull;
-		for (int invSlot = 0; invSlot < invObjects.size(); ++invSlot)
-			if (invObjects.at(invSlot) == 0)
+		for (int invSlot = 0; invSlot < inventory.size(); ++invSlot)  // Search through players inventory to determine if it's full
+			if (inventory.at(invSlot) == ItemList::GENERIC_HAND)  // GENERIC_HAND is used as the placeholder value
 			{
 				invIsFull = false;
-				break;
 			}
 			else
 			{
 				invIsFull = true;
 			}
-		if (invIsFull == false)
+		if (invIsFull == false)  // Runs to open slot and places new item in it
 		{
-			for (int invSlot = 0; invSlot < invObjects.size(); ++invSlot)
-				if (invObjects.at(invSlot) == 0)
-					invObjects.at(invSlot) = objID;
+			for (int invSlot = 0; invSlot < inventory.size(); ++invSlot)
+				if (inventory.at(invSlot) == ItemList::GENERIC_HAND)
+				{
+					inventory.at(invSlot) = item;
+					std::cout << "You added the " << ItemsToStrings(item) << " to your inventory.\n";
+				}
 		}
-		else
+		else  // If the player's inventory is full, then tell them this and run the ReplaceItemInInv funtion (see above)
 		{
-			std::cout << "Sorry, you don't have an open inventory slot for this.\n";
-			std::cout << "Please enter a slot to replace this item. The item in that slot will be destroyed.\n";
-			std::cout << "Slot: ";
-			int slot;
-			std::cin >> slot;
-			std::cout << "\n";
-			invObjects.at(slot) = objID;
+			std::cout << "Sorry, you do not have an open inventory slot for this.\n";
+			ReplaceItemInInv(item);
 		}
 	}
 
