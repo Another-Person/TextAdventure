522481449064cdc6728fde342d80b83381258604
Joey Sachtleben <manjoey7@gmail.com>
2018-08-15 16:16:33 -0500

    Created core files for Map Update

---
+++ TextAdventure/Map.cpp
@@ -0,0 +1,7 @@
+/* Map.cpp
+ * Last editied with 0.3.0
+ * Holds code for the Map class and related code.
+ */
+
+
+#include "stdafx.h"
---
+++ TextAdventure/Map.h
@@ -0,0 +1,10 @@
+/* Map.h
+ * Last updated with 0.3.0
+ * Provides the Map class and related code.
+ */
+
+#ifndef MAP_H
+#define MAP_H
+
+
+#endif // !MAP_H
